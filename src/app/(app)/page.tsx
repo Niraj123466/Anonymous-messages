4483eeb0d3c11e99236fa283cf298b21a97138db
Niraj More <moreniraj49@gmail.com>
2024-08-17 22:35:53 -0700

    footer updated

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -60,7 +60,7 @@ export default function Home() {
 
       {/* Footer */}
       <footer className="text-center p-4 md:p-6 bg-gray-900 text-white">
-        © 2023 True Feedback. All rights reserved.
+        © 2024 True Feedback.  All rights reserved.
       </footer>
     </>
   );
--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -9,7 +9,7 @@ export async function sendVerificationEmail(
 ): Promise<ApiResponse> {
   try {
     await resend.emails.send({
-      from: 'dev@hiteshchoudhary.com',
+      from: 'Acme <onboarding@resend.dev>',
       to: email,
       subject: 'Mystery Message Verification Code',
       react: VerificationEmail({ username, otp: verifyCode }),
